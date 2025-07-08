225b3a5198bc2f1392ec9651ab99063820de0837
Ngọc Bình Nguyễn <nnbinh0301@gmail.com>
2025-07-08 07:03:38 +0700

    ds system

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import CollectionSection from './components/CollectionSection';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DSDashboardScreen from './design-system/showcases/DSDashboardScreen';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<AboutSection />} />
-          <Route path="/about" element={<AboutSection />} />
-          <Route path="/collection" element={<CollectionSection />} />
-          <Route path="/contact" element={<ContactSection />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <DSDashboardScreen />
   );
+  // return (
+  //   <BrowserRouter>
+  //     <div className="App">
+  //       <Header />
+  //       <Routes>
+  //         <Route path="/" element={<AboutSection />} />
+  //         <Route path="/about" element={<AboutSection />} />
+  //         <Route path="/collection" element={<CollectionSection />} />
+  //         <Route path="/contact" element={<ContactSection />} />
+  //         {/* <Route path="/design-system" element={<DSDashboardScreen />} /> */}
+  //       </Routes>
+  //       <Footer />
+  //     </div>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
